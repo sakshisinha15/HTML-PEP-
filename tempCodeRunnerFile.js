bb9906df32cd92294be64a1afe6593f63f90cab3
sakshisinha15 <sakshisinha2854@gmail.com>
2026-01-28 11:19:21 +0530

    ALL the HTML CSS AND JAVASCRIPT CODE

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,31 @@
+// let str="bahu bali"
+// console.log(str.length)
+// console.log(str.toUpperCase());
+// console.log(str.slice(3));
+// console.log(str.slice(3,6));
+// console.log(str.replace("hu","aa"));
+// console.log(str.concat("5"));
+// console.log(str.trim());
+
+
+
+
+// let arr=[1,2,3,4,5,7,6,8,9]
+// console.log(arr.toString());
+// console.log(typeof(arr));
+// console.log(arr.join(0));
+// console.log(arr.push(7));
+// console.log(arr.toString());
+// console.log(arr.pop(9));
+// console.log(arr.toString());
+// console.log(arr.shift());
+// console.log(arr.unshift());
+// console.log(arr.concat(23));
+// console.log(arr.sort());
+// console.log(arr.splice(1,1,6));
+// console.log(arr.reverse());
+// console.log(arr.map(x=>x*2));
+let str="bahu bali"
+console.log("hello")
+let a=document.getElementById("selectid")
+console.log(a);
